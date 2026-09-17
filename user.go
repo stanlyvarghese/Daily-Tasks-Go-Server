3a54745033bf678b4d88394b2755b3db2d2e400c
stanlyvarghese <stanlyvarghese14@gmail.com>
2026-09-18 00:29:32 +0530

    User.go to act as the user database layer and insert data like username, email, hashcode for password and createdion time.

--- user.go
+++ user.go
@@ -1,9 +1,34 @@
 package main
 
+import "time"
+
 type User struct {
-	ID       int64
-	Username string
-	Email    string
-	Password string
-	CreatedAt string
+    ID           int64
+    Username     string
+    Email        string
+    PasswordHash string
+    CreatedAt    string
+}
+
+func (s *Server) CreateUser(
+    username string,
+    email string,
+    passwordHash string,
+) error {
+    _, err := s.db.Exec(`
+        INSERT INTO users (
+            username,
+            email,
+            password_hash,
+            created_at
+        )
+        VALUES (?, ?, ?, ?)
+    `,
+        username,
+        email,
+        passwordHash,
+        time.Now().UTC().Format(time.RFC3339),
+    )
+
+    return err
 }
